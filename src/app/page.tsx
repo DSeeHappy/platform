242bfda90744b65305b29d4d3419dae978622cb8
Daniel Sanchez <ds@dseehappy>
2024-03-23 21:43:58 -0600

    init pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+'use client'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-    </main>
-  );
+    return (
+        <>
+            <h1 className="text-black">test</h1>
+        </>
+    )
 }
---
+++ src/app/users/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+const meta = {
+    title: 'Users',
+    description: 'Users page',
+}
+export default function UsersPage() {
+    return (
+        <>
+            <h1 className="text-black">test test</h1>
+        </>
+    )
+}
